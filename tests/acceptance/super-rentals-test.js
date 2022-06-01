f7cf6395e89300196652249ccbc7c20322733d30
arafat-dexcom <arafat.iqbal@dexcom.com>
2022-05-31 17:27:41 -0700

    Added rental properties, and a navbar

--- tests/acceptance/super-rentals-test.js
+++ tests/acceptance/super-rentals-test.js
@@ -7,8 +7,9 @@ module('Acceptance | super rentals', function (hooks) {
   test('visiting /', async function (assert) {
     await visit('/');
     assert.strictEqual(currentURL(), '/');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('SuperRentals');
     assert.dom('h2').hasText('Welcome to Super Rentals!');
-
     assert.dom('.jumbo a.button').hasText('About Us');
     await click('.jumbo a.button');
 
@@ -19,8 +20,9 @@ module('Acceptance | super rentals', function (hooks) {
     await visit('/about');
 
     assert.strictEqual(currentURL(), '/about');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('SuperRentals');
     assert.dom('h2').hasText('About Super Rentals');
-
     assert.dom('.jumbo a.button').hasText('Contact Us');
     await click('.jumbo a.button');
 
@@ -31,11 +33,29 @@ module('Acceptance | super rentals', function (hooks) {
     await visit('/getting-in-touch');
 
     assert.strictEqual(currentURL(), '/getting-in-touch');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('SuperRentals');
     assert.dom('h2').hasText('Contact Us');
-
     assert.dom('.jumbo a.button').hasText('About');
     await click('.jumbo a.button');
 
     assert.strictEqual(currentURL(), '/about');
+  });
+  test('navigating using the nav-bar', async function (assert) {
+    await visit('/');
+
+    assert.dom('nav').exists();
+    assert.dom('nav a.menu-index').hasText('SuperRentals');
+    assert.dom('nav a.menu-about').hasText('About');
+    assert.dom('nav a.menu-contact').hasText('Contact');
+
+    await click('nav a.menu-about');
+    assert.strictEqual(currentURL(), '/about');
+
+    await click('nav a.menu-contact');
+    assert.strictEqual(currentURL(), '/getting-in-touch');
+
+    await click('nav a.menu-index');
+    assert.strictEqual(currentURL(), '/');
   });
 });
