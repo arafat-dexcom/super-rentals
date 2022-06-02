a4fc8b501b11fe935cd0d2f23de46d958a8b125c
arafat-dexcom <arafat.iqbal@dexcom.com>
2022-06-02 15:01:00 -0700

    Added map functionality + tests

--- config/environment.js
+++ config/environment.js
@@ -47,5 +47,7 @@ module.exports = function (environment) {
     // here you can enable a production-specific feature
   }
 
+  ENV.MAPBOX_ACCESS_TOKEN =
+    'pk.eyJ1IjoiYXJhZmF0aXExMjMiLCJhIjoiY2wzeGk0dHduMDExeTNlcGoxNDJ2cXJ3ZCJ9.EJvfqzstTbo4Ns8_YCWROA';
   return ENV;
 };
